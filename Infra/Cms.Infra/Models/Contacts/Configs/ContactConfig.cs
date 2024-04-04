97db61e1ca5ce4ee81e91c659447d2899186f7a8
yasin abedini <yasinabedini1384@gmail.com>
2024-04-04 10:41:55 +0330

    feat = add contact repository and configs

---
+++ Infra/Cms.Infra/Models/Contacts/Configs/ContactConfig.cs
@@ -0,0 +1,20 @@
+﻿using Cms.Domain.Common.ValueObjects.Conversion;
+using Cms.Domain.Models.Contact.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Infra.Models.Contacts.Configs
+{
+    public class ContactConfig : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.Property(t => t.Text).HasConversion<DescriptionConversion>();
+        }
+    }
+}
---
+++ Infra/Cms.Infra/Models/Contacts/Repositories/ContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cms.Domain.Models.Contact.Entities;
+using Cms.Domain.Models.Contact.Repositories;
+using Cms.Infra.Common.Repository;
+using Cms.Infra.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Infra.Models.Contacts.Repositories
+{
+    public class ContactRepository : BaseRepository<Contact>, IContactRepository
+    {
+        public ContactRepository(CmsDbContext context) : base(context)
+        {
+        }
+    }
+}
