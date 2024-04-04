bda5823846f50ffdf376570970de4cd42a3685f8
yasin abedini <yasinabedini1384@gmail.com>
2024-04-04 10:42:18 +0330

    feat - add info repository and configs

---
+++ Infra/Cms.Infra/Models/Info/Configs/InfoConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Infra.Models.Info.Configs
+{
+    public class InfoConfig : IEntityTypeConfiguration<Domain.Models.Info.Entities.Info>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Models.Info.Entities.Info> builder)
+        {
+            var defaultInfo = Domain.Models.Info.Entities.Info.Create("", "", "00-00000000", "aaaa@domain.com", "");
+            defaultInfo.SetId(1);
+
+            builder.HasData(defaultInfo);
+        }
+    }
+}
---
+++ Infra/Cms.Infra/Models/Info/Reositories/InfoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Cms.Domain.Models.Info.Repositories;
+using Cms.Infra.Common.Repository;
+using Cms.Infra.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Infra.Models.Info.Reositories
+{
+    public class InfoRepository : BaseRepository<Domain.Models.Info.Entities.Info>, IInfoRepository
+    {
+        public InfoRepository(CmsDbContext context) : base(context)
+        {
+        }
+    }
+}
