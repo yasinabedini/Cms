6d49013c0b8aab7e33fe9ec13ff594ca43d9651e
yasin abedini <yasinabedini1384@gmail.com>
2024-04-09 12:44:57 +0330

    fix - fix some bugs from identity

--- EndPoints/Cms.Endpoints.Identity/buildschema.bat
+++
@@ -1,3 +0,0 @@
-rmdir /S /Q "Data/Migrations"
-
-dotnet ef migrations add Users -c CmsDbContext -o Data/Migrations
--- EndPoints/Cms.Endpoints.Identity/buildschema.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf "Data/Migrations"
-
-dotnet ef migrations add Users -c CmsDbContext -o Data/Migrations
