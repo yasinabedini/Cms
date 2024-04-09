fb9ed330c1a89a2c551d789b9ec2a1038c8f4217
yasin abedini <yasinabedini1384@gmail.com>
2024-04-10 01:11:04 +0330

    feat - add filemanager Api to project

---
+++ FileManager/FileManager/Controllers/FileManagerController.cs
@@ -0,0 +1,51 @@
+﻿using FileManager.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileManagerController : ControllerBase
+    {
+
+        [HttpPost("Upload")]
+        public IActionResult Upload([FromForm] IFormFile image, string folder)
+        {
+            if (image is null)
+            {
+                return NotFound();
+            }
+
+
+            if (folder is not "news" && folder is not "sweeper")
+            {
+                return BadRequest("Invalid Folder Name.");
+            }
+
+
+            int maxLength = 5 * 1024 * 1025;
+            if (image.Length > maxLength)
+            {
+                return BadRequest("the max of image size is 5 MG.");
+            }
+
+            string extension = Path.GetExtension(image.FileName).ToLower();
+            if (extension.EndsWith(".png") || extension.EndsWith(".jpeg") || extension.EndsWith(".jpg"))
+            {
+                string imageName = Guid.NewGuid().ToString() + extension;
+
+                FileTools.SaveImage(image, imageName, folder, false);
+
+                return Ok(imageName);
+            }
+            else
+            {
+                return BadRequest("Invalid Image Format.");
+            }
+
+
+
+        }
+    }
+}
---
+++ FileManager/FileManager/Tools/FileTools.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO.Pipelines;
+using System.Net;
+
+namespace FileManager.Tools;
+
+public static class FileTools
+{
+    public static void SaveImage(IFormFile profileImage, string imageName, string whichFolder, bool thumbSave)
+    {
+        string imagePath = imagePath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\img\\{whichFolder}", imageName);
+
+        using (var stream = new FileStream(imagePath, FileMode.Create))
+        {
+            profileImage.CopyTo(stream);
+        }
+
+        ////save ThumbNail
+        //if (thumbSave)
+        //{
+        //    string thumbImagePath = Path.Combine(Directory.GetCurrentDirectory().Replace("Application\\Cmd.Application\\Tools\\", "EndPoints\\Cms.Endpoints.Site"), $"wwwroot\\img\\{whichFolder}\\Thumb", imageName);
+        //    ImageConvertor imageConvertor = new ImageConvertor();
+        //    imageConvertor.Image_resize(imagePath, thumbImagePath, 213);
+        //}
+    }
+    public static void DeleteFile(string folder, string imageName)
+    {
+        string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\img\\{folder}", imageName);
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+}
