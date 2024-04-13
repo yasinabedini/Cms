8cbf21e4951acb1c9d45b002ef1cb38a5a204f54
yasin abedini <yasinabedini1384@gmail.com>
2024-04-13 14:00:24 +0330

    fix - fix some bugs

--- FileManager/FileManager/Program.cs
+++ FileManager/FileManager/Program.cs
@@ -15,6 +15,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
