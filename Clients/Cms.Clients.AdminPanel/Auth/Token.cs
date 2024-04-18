0ebd299fafc05741bae929827ecbc5c6410dff3a
yasin abedini <yasinabedini1384@gmail.com>
2024-04-19 00:39:44 +0330

    feat - add token request and response

---
+++ Clients/Cms.Clients.AdminPanel/Auth/Token.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+
+namespace Cms.Clients.AdminPanel.Auth
+{
+    public static class Token
+    {
+        public async static Task<TokenResponse> GetTokenResponse(HttpClient httpClient,HttpContext context)
+        {
+            IConfiguration configuration;
+
+            configuration =  context.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
+
+            var discovery = await httpClient.GetDiscoveryDocumentAsync(configuration.GetSection("AuthorityUrl").Value);
+            if (discovery.IsError)
+            {
+
+            }
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discovery.TokenEndpoint,
+                ClientId = "admin",
+                ClientSecret = "adminSecret",
+                Scope = "api.admin"
+            });
+            if (tokenResponse.IsError)
+            {
+
+            }
+
+            return tokenResponse;
+        }
+    }
+}
