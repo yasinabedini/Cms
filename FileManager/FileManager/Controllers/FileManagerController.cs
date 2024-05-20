139f46046ea21dd2bd5038a31bbd0e8b1de7af93
yasin abedini <yasinabedini1384@gmail.com>
2024-05-20 16:02:10 +0330

    fix - fix bugs from fileManager

--- FileManager/FileManager/Controllers/FileManagerController.cs
+++ FileManager/FileManager/Controllers/FileManagerController.cs
@@ -1,6 +1,7 @@
 ﻿using FileManager.Tools;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Diagnostics.Eventing.Reader;
 using System.Text.Json;
 
@@ -11,35 +12,93 @@ namespace FileManager.Controllers
     public class FileManagerController : ControllerBase
     {
         [HttpPost("Upload")]
-        public IActionResult Upload([FromForm] IFormFile image, string folder)
+        public IActionResult Upload([FromForm] IFormFile file, string folder)
         {
-            if (image is null)
+            if (file is null)
             {
                 return NotFound();
             }
 
 
-            if (folder is not "news" && folder is not "sweeper")
+            if (folder is not "news" && folder is not "sweeper" && folder is not "gallery" && folder is not "attachment")
             {
                 return BadRequest("Invalid Folder Name.");
             }
 
-
             int maxLength = 5 * 1024 * 1025;
-            if (image.Length > maxLength)
+            if (file.Length > maxLength)
             {
                 return BadRequest("the max of image size is 5 MG.");
             }
 
-            string extension = Path.GetExtension(image.FileName).ToLower();
-            if (!extension.EndsWith(".png") && !extension.EndsWith(".jpeg") && !extension.EndsWith(".jpg"))
-            {
-                return BadRequest("Invalid Image Format.");
-            }
+            string extension = Path.GetExtension(file.FileName).ToLower();
 
             string imageName = Guid.NewGuid().ToString() + extension;
 
-            FileTools.SaveImage(image, imageName, folder, false);
+            if (folder is "attachment")
+            {
+                string type = "";
+                if (extension is ".png" || extension is ".jpg" || extension is ".jpeg")
+                {
+                    type = "image";
+                }
+                if (extension is ".mp4" || extension is ".mkv")
+                {
+                    type = "video";
+                }
+                if (extension is ".pdf" || extension is ".zip" || extension is ".rar" || extension is ".txt" || extension is ".docx" || extension is ".pptx" || extension is ".xlsx")
+                {
+                    type = "document";
+                }
+                if (extension is ".mp3" || extension is ".ogg")
+                {
+                    type = "voice";
+                }
+
+
+                string imagePath = imagePath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\img\\attachment\\{type}", imageName);
+
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            else
+            {
+                FileTools.SaveImage(file, imageName, folder, false);
+            }
+
+            var imageNameJson = JsonSerializer.Serialize(imageName);
+
+            Response.Headers.Add("fileName", imageName);
+            return Ok();
+        }
+
+        [HttpPost("UploadFile")]
+        public IActionResult UploadFile([FromForm] IFormFile file)
+        {
+            if (file is null)
+            {
+                return NotFound();
+            }
+
+
+            int maxLength = 5 * 1024 * 1025;
+            if (file.Length > maxLength)
+            {
+                return BadRequest("the max of image size is 5 MG.");
+            }
+
+            string extension = Path.GetExtension(file.FileName).ToLower();
+
+            string imageName = Guid.NewGuid().ToString() + extension;
+
+            string imagePath = imagePath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\img\\gallery\\", imageName);
+
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
 
             var imageNameJson = JsonSerializer.Serialize(imageName);
 
@@ -50,7 +109,7 @@ namespace FileManager.Controllers
         [HttpGet("GetImage")]
         public IActionResult GetImage(string imageName, string folder)
         {
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","img", folder, imageName);
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", folder, imageName);
 
             if (!Path.Exists(filePath)) return BadRequest();
 
@@ -59,15 +118,57 @@ namespace FileManager.Controllers
         }
 
         [HttpGet("GetFile")]
-        public IActionResult GetFile(string fileName,string type)
+        public IActionResult GetFile(string fileName, int type)
         {
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","gallery",type, fileName);
+            string typeStr;
+            string mimType = "";
+            if (type is 3)
+            {
+                typeStr = "image";
+                mimType = "image/jpeg";
+            }
+            else if (type is 5)
+            {
+                typeStr = "document";
+                mimType = "application/pdf";
+            }
+            else if (type is 4)
+            {
+                typeStr = "video";
+                mimType = "video/mp4";
+            }
+            else if (type is 6)
+            {
+                typeStr = "voice";
+                mimType = "audio/mpeg";
+            }
+            else
+            {
+                typeStr = "";
+                mimType = "";
+            }
 
-            if (!Path.Exists(filePath)) return BadRequest();
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "attachment", typeStr, fileName);
 
-            byte[] imageBytes = System.IO.File.ReadAllBytes($@"wwwroot/Gallery/{type}/{fileName}");
+            if (!Path.Exists(filePath))
+            {
+                filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "gallery", fileName);
+                if (!Path.Exists(filePath)) return BadRequest();
+            }
 
-            return File(imageBytes, "image/jpeg");
+            byte[] imageBytes = System.IO.File.ReadAllBytes(filePath);
+
+           
+
+
+            if (type is 3)
+            {
+                return File(imageBytes, mimType);
+            }
+            else
+            {
+                return File(imageBytes, mimType,fileName);
+            }
         }
 
         [HttpDelete("Delete")]
