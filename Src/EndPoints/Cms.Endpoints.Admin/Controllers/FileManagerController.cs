483e3b91c711f3a5838e84590ed01d7a549dddc2
yasin abedini <yasinabedini1384@gmail.com>
2024-05-25 11:53:07 +0330

    fix - fix some bugs from fileManager

--- Src/EndPoints/Cms.Endpoints.Admin/Controllers/FileManagerController.cs
+++ Src/EndPoints/Cms.Endpoints.Admin/Controllers/FileManagerController.cs
@@ -7,6 +7,7 @@ using System.IO.Pipelines;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Cms.Endpoints.Admin.Controllers
@@ -21,31 +22,8 @@ namespace Cms.Endpoints.Admin.Controllers
             _FileManager = factory.CreateClient("FileManager");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Upload(IFormFile image, string folder)
-        {
-            var requestContent = new MultipartFormDataContent();
-
-            var item = new MemoryStream();
-            image.CopyTo(item);
-            item.Position = 0;
-
-            // Assuming you have the image path            
-            var imageContent = new ByteArrayContent(item.ToArray());
-            imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
-
-            // 'file' corresponds to the name of the form field in your API
-            requestContent.Add(imageContent, "image", Path.GetFileName(image.FileName));
-
-            // Replace 'your_api_endpoint' with the actual endpoint where you want to send the image
-            var response = await _FileManager.PostAsync($"/api/FileManager/upload?folder={folder}", requestContent);
-
-            item.Dispose();
-            return Ok(response.Headers.First(t => t.Key == "imageName").Value);
-        }
-
-        [HttpPost("UploadFile")]
-        public async Task<IActionResult> UploadFile(IFormFile file, string type)
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload(IFormFile file, string folder)
         {
             var requestContent = new MultipartFormDataContent();
 
@@ -58,13 +36,38 @@ namespace Cms.Endpoints.Admin.Controllers
             imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
 
             // 'file' corresponds to the name of the form field in your API
-            requestContent.Add(imageContent, "image", Path.GetFileName(file.FileName));
+            requestContent.Add(imageContent, "file", Path.GetFileName(file.FileName));
 
             // Replace 'your_api_endpoint' with the actual endpoint where you want to send the image
-            var response = await _FileManager.PostAsync($"/api/FileManager/UploadFileToGallery?type={type}", requestContent);
+            var response =await _FileManager.PostAsync($"/api/FileManager/upload?folder={folder}", requestContent);
+
+            var imageName = response.Headers.First(t => t.Key == "fileName").Value.First();
+            
+            Response.Headers.Add("fileName",imageName);            
+            return Ok(imageName);
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var requestContent = new MultipartFormDataContent();
+
+            var item = new MemoryStream();
+            file.CopyTo(item);
+            item.Position = 0;
+
+            // Assuming you have the image path            
+            var imageContent = new ByteArrayContent(item.ToArray());
+            imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+
+            // 'file' corresponds to the name of the form field in your API
+            requestContent.Add(imageContent, "file", Path.GetFileName(file.FileName));
+
+            // Replace 'your_api_endpoint' with the actual endpoint where you want to send the image
+            var response = await _FileManager.PostAsync($"/api/FileManager/UploadFileToGallery", requestContent);
 
             item.Dispose();
-            return Ok(response.Headers.First(t => t.Key == "imageName").Value);
+            return Ok(response.Headers.First(t => t.Key == "fileName").Value);
         }
 
         [HttpGet("GetImage")]
@@ -120,7 +123,7 @@ namespace Cms.Endpoints.Admin.Controllers
             }
         }
 
-        [HttpDelete("DeleteImage")]
+        [HttpDelete("Delete")]
         public async Task<IActionResult> DeleteImage(string imageName, string folder)
         {
             var response = await _FileManager.DeleteAsync($"api/FileManager/Delete?imageName={imageName}&&folder={folder}");
