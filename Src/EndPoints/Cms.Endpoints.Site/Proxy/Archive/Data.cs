f651fe7c921e5897f0620cbfc084a88c47c034d1
yasin abedini <yasinabedini1384@gmail.com>
2024-05-25 11:52:41 +0330

    feat - Add Proxy viewModels

---
+++ Src/EndPoints/Cms.Endpoints.Site/Proxy/Archive/Data.cs
@@ -0,0 +1,8 @@
+﻿namespace Cms.Endpoints.Site.Proxy.Archive
+{
+    public class Data
+    {
+        public List<Item> items { get; set; }
+        public int totalItems { get; set; }   
+}
+}
---
+++ Src/EndPoints/Cms.Endpoints.Site/Proxy/FinalView/Root.cs
@@ -0,0 +1,11 @@
+﻿namespace Cms.Endpoints.Site.Proxy.FinalView
+{
+    public class Root
+    {
+        public FinalView.Data data { get; set; }
+        public bool isSuccess { get; set; }
+        public int statusCode { get; set; }
+        public string messages { get; set; }
+        public object errors { get; set; }
+    }
+}
